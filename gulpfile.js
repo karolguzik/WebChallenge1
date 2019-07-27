865f569f429628c250d0d6fb0236c782dfdf5ab9
Karol Guzik <guzik912@o2.pl>
2019-07-27 21:26:28 +0200

    Folders and files structure with some code

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const minifyCss = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const imageMin = require('gulp-imagemin');
+
+function copyHtml() {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+}
+
+function style() {
+  return gulp.src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+}
+
+
+function scripts() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('src/scss/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('src/js/*.js').on('change', browserSync.reload);
+}
+
+exports.copyHtml = copyHtml;
+exports.style = style;
+exports.scripts = scripts;
+exports.watch = watch;
